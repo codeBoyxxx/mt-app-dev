9161eead7ae569b5649a5cebdfad952a4d3873a1
王蒙 <ex-wangm003>
2018-11-29 08:06:53 +0800

    页面模板创建完成

---
+++ components/public/header/geo.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="m-geo">
+        <i class="el-icon-location" /> 北京市
+        <nuxt-link class="changeCity" to='changeCity'>切换城市</nuxt-link>
+        [廊坊 香河 天津]
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<script>
+export default {
+    
+}
+</script>
+
+
